cbd6493e512da0cb1f75b6c200c2c3dd1384129f
Aman Vishwakarma <aman.vishwakarma@infytrix.com>
2025-12-04 12:13:48 +0530

    version 1.

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.use(cookieParser());
 // });
 
 app.get('/', (req, res, next) => {
-  res.json({ message: 'Server is running' });
+  res.json({ message: 'Server is running v1' });
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
